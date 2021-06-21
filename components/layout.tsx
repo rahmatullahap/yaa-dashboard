60fbef830eec3fb72f8f6c0b3a72561a207642d6
Rahmatullah Arrizal <rahmatullaharrizal@gmail.com>
2021-06-21 10:56:45 +0700

    init commit

---
+++ components/layout.tsx
@@ -0,0 +1,120 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = '[Your Name]'
+export const siteTitle = 'Next.js Sample Website'
+
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta name='application-name' content='PWA App' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content='PWA App' />
+        <meta name='description' content='Best PWA App in the world' />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
+        <meta name='msapplication-TileColor' content='#2B5797' />
+        <meta name='msapplication-tap-highlight' content='no' />
+        <meta name='theme-color' content='#000000' />
+
+        <link rel='apple-touch-icon' href='/static/icons/touch-icon-iphone.png'/>
+        <link rel='apple-touch-icon' sizes='152x152' href='/static/icons/touch-icon-ipad.png'/>
+        <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/touch-icon-iphone-retina.png'/>
+        <link rel='apple-touch-icon' sizes='167x167' href='/static/icons/touch-icon-ipad-retina.png'/>
+
+        <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
+        <link rel='manifest' href='/static/manifest.json' />
+        <link rel='mask-icon' href='/static/icons/safari-pinned-tab.svg' color='#5bbad5' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
+            
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:url' content='https://yourdomain.com' />
+        <meta name='twitter:title' content='PWA App' />
+        <meta name='twitter:description' content='Best PWA App in the world' />
+        <meta name='twitter:image' content='https://yourdomain.com/static/icons/android-chrome-192x192.png' />
+        <meta name='twitter:creator' content='@DavidWShadow' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='PWA App' />
+        <meta property='og:description' content='Best PWA App in the world' />
+        <meta property='og:site_name' content='PWA App' />
+        <meta property='og:url' content='https://yourdomain.com' />
+        <meta property='og:image' content='https://yourdomain.com/static/icons/apple-touch-icon.png' />
+
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
+}
